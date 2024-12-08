26b8b35c4eb25cfc6f9124335e53cd9e89e75be7
Cooper Mann <coop530@comcast.net>
2024-12-07 21:59:04 -0500

    Water sensor done, getting GPS and IMU data

--- ros2_ws/src/test_input/test_input/control.py
+++ ros2_ws/src/test_input/test_input/control.py
@@ -1,13 +1,26 @@
 import rclpy
 from rclpy.node import Node
+import os
 
-from std_msgs.msg import String
+from std_msgs.msg import Bool
 
 class Control(Node):
 
     def __init__(self):
         super().__init__('control')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+
+        # Subscriber for water sensor
+        self.create_subscription(Bool, 'water_detected', self.detect_water, 10)
+        
+        self.get_logger().info('Initialized control node')
+
+    def detect_water(self, msg):
+        if(msg.data):
+            #Shutdown everything
+            self.get_logger().warn("Emergency Stop Received! Shutting down all nodes...")
+            os.system('ros2 lifecycle set / all shutdown')  # For managed nodes
+            os.system('killall -9 ros2')
+
         
 
 
