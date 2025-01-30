7a92fe5da7625ddbbd172fe80c28adcfaf097664
Renata Kaplan <renkap7747@gmail.com>
2025-01-30 14:54:00 -0500

    small change

--- ros2_ws/src/test_input/test_input/motor_controller.py
+++ ros2_ws/src/test_input/test_input/motor_controller.py
@@ -33,9 +33,8 @@ class MotorControllerNode(Node):
         # self.left_motor.value = self.MAX_PWM
         # self.left_motor.value = self.MIN_PWM
 
-        self.left_motor.value = self.MID_PWM
-
-        
+        # self.left_motor.value = self.MID_PWM
+        self.left_motor.value.blink(on_time=0.0015, off_time=0.9985, fade_in_time=0, fade_out_time=0, n=1000, background=True)        
 
 
         self.motor_subscription = self.create_subscription(Float64MultiArray, 'set_motor_vels', self.hard_code, 10)
