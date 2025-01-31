f660e188fc1ccaf601ee8c700eee7dabe8533d99
jhkeselman <jhkeselman@wpi.edu>
2025-01-31 10:02:38 -0500

    trying another way

--- ros2_ws/src/test_input/test_input/motor_controller.py
+++ ros2_ws/src/test_input/test_input/motor_controller.py
@@ -28,13 +28,13 @@ class MotorControllerNode(Node):
 
         # Test if ESC is connected
         self.left_motor.blink(on_time=3, off_time=3, fade_in_time=0, fade_out_time=0, n=2, background=False)
-        rclpy.sleep(1)
+        time.sleep(1)
 
         # ARMING SEQUENCE
         # self.left_motor.value = self.MAX_PWM
         # self.left_motor.value = self.MIN_PWM
 
-        self.left_motor._write(self.MID_PWM)
+        self.left_motor.value = self.MID_PWM
         # self.left_motor.value = 0.0
         # self.left_motor.blink(on_time=0.0015, off_time=0.9985, fade_in_time=0, fade_out_time=0, n=None, background=True)        
 
