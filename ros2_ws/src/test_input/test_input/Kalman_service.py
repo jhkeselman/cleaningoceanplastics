0983fc4ff5a51244ac17e23add9d600e38691222
John Hall <jkhall@wpi.edu>
2025-02-16 16:13:56 -0500

    finally got service to work

--- ros2_ws/src/test_input/test_input/Kalman_service.py
+++ ros2_ws/src/test_input/test_input/Kalman_service.py
@@ -1,7 +1,9 @@
+from services.srv import KalmanState
+
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Float64
 
 import math
 import numpy as np
@@ -11,11 +13,16 @@ class KalmanService(Node):
     def __init__(self):
         super().__init__('Kalman_service')
         self.srv = self.create_service(KalmanState, 'get_Kalman_state', self.return_state)
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def return_state(self, request, response):
         response.state = np.zeros(5)
         self.get_logger().info('Incoming request')
         return response
+    
+    def timer_callback(self):
+        pass
 
 
 def main():
