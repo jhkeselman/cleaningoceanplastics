2ab92286901ea1c73e7b7a774fee42bcdde76cf3
John Hall <jkhall@wpi.edu>
2025-02-17 22:07:25 -0500

    loop read imu client

--- ros2_ws/src/test_input/test_input/imu_client.py
+++ ros2_ws/src/test_input/test_input/imu_client.py
@@ -13,21 +13,27 @@ class IMUClient(Node):
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
         self.req = IMUData.Request()
+        timer_period = 1
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def send_request(self):
         self.future = self.cli.call_async(self.req)
         rclpy.spin_until_future_complete(self, self.future)
         return self.future.result()
+    
+    def timer_callback(self):
+        response = self.send_request()
+        self.get_logger().info(
+        'IMU Heading %5.3f, Acc %5.3f, Omega %5.3f:' %(response.heading, response.acceleration, response.omega))
+
 
 
 def main(args=None):
     rclpy.init(args=args)
 
     imu_client = IMUClient()
-    response = imu_client.send_request()
-    imu_client.get_logger().info(
-        'IMU Heading %5.3f, Acc %5.3f, Omega %5.3f:' %(response.heading, response.acceleration, response.omega))
-
+    rclpy.spin(imu_client)
+    
     imu_client.destroy_node()
     rclpy.shutdown()
 
