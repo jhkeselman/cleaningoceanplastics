b70451736f6c2b89fbd829b4ebf82a86066deda1
jhkeselman <jhkeselman@wpi.edu>
2025-02-21 15:53:14 -0500

    small numbas make it go

--- ros2_ws/src/test_input/test_input/teleop.py
+++ ros2_ws/src/test_input/test_input/teleop.py
@@ -19,35 +19,34 @@ class Teleop(Node):
 
     def check_input(self):
         key = sys.stdin.read(1)
-        self.get_logger().info('Key: ' + key)
         if key == 'w':
             if key == self.last_input:
-                self.left_value = min(10, self.left_value + 0.5)
-                self.right_value = min(10, self.right_value + 0.5)
+                self.left_value = max(5, self.left_value - 0.5)
+                self.right_value = max(5, self.right_value - 0.5)
             else:
-                self.left_value = 8.0
-                self.right_value = 8.0
+                self.left_value = 6.0
+                self.right_value = 6.0
         elif key == 's':
             if key == self.last_input:
-                self.left_value = max(5, self.left_value - 0.5)
-                self.right_value = max(5, self.right_value - 0.5)
-            else:
-                self.left_value = 6.0
-                self.right_value = 6.0
-        elif key == 'a': # Turn left
-            if key == self.last_input:
-                self.left_value = max(5, self.left_value - 0.5)
+                self.left_value = min(10, self.left_value + 0.5)
                 self.right_value = min(10, self.right_value + 0.5)
             else:
-                self.left_value = 6.0
+                self.left_value = 8.0
                 self.right_value = 8.0
-        elif key == 'd': # Turn Right
+        elif key == 'a': # Turn left
             if key == self.last_input:
                 self.left_value = min(10, self.left_value + 0.5)
                 self.right_value = max(5, self.right_value - 0.5)
             else:
                 self.left_value = 8.0
                 self.right_value = 6.0
+        elif key == 'd': # Turn Right
+            if key == self.last_input:
+                self.left_value = max(5, self.left_value - 0.5)
+                self.right_value = min(10, self.right_value + 0.5)
+            else:
+                self.left_value = 6.0
+                self.right_value = 8.0
         elif key == 'x':
             self.left_value = 7.5
             self.right_value = 7.5
