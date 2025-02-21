933326d79c4d235baf7e9bedcf8cda2983b6fd6d
jhkeselman <jhkeselman@wpi.edu>
2025-02-21 15:20:40 -0500

    launch file

--- ros2_ws/src/test_input/launch/teleop_launch.py
+++ ros2_ws/src/test_input/launch/teleop_launch.py
@@ -4,6 +4,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(package='test_input', executable='teleop', output='screen'),
+        Node(package='test_input', executable='motor_control', output='screen'),
         Node(package='test_input', executable='control', output='screen'),
         Node(package='test_input', executable='imu_srv', output='screen'),
         Node(package='test_input', executable='water_sensor', output='screen'),
