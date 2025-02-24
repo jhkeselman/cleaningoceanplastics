967019df8af3ac18fb2a54e13d009cfa53d5bb1f
Cooper Mann <coop530@comcast.net>
2025-02-24 14:32:35 -0500

    updated some more names

--- ros2_ws/src/sensors/launch/teleop_launch.py
+++ ros2_ws/src/sensors/launch/teleop_launch.py
@@ -3,6 +3,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='test_input', executable='teleop', output='screen'),
-        Node(package='test_input', executable='motor_control', output='screen'),
+        Node(package='sensors', executable='teleop', output='screen'),
+        Node(package='sensors', executable='motor_control', output='screen'),
     ])
