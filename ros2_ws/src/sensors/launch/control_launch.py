58ec5e71fba78d31930711a2a0b19f4d5741bd2e
Cooper Mann <coop530@comcast.net>
2025-02-25 14:08:47 -0500

    Restructured nodes

--- ros2_ws/src/sensors/launch/control_launch.py
+++ ros2_ws/src/sensors/launch/control_launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        Node(package='sensors', executable='control', output='screen'),
+        Node(package='navigation', executable='control', output='screen'),
         Node(package='sensors', executable='imu_srv', output='screen'),
         Node(package='sensors', executable='water_sensor', output='screen'),
         Node(package='nmea_navsat_driver', executable='nmea_serial_driver', output='screen'),
--- ros2_ws/src/sensors/launch/teleop_launch.py
+++
@@ -1,8 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package='sensors', executable='teleop', output='screen'),
-        Node(package='sensors', executable='motor_control', output='screen'),
-    ])
