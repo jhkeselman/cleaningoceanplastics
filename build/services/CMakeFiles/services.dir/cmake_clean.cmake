b4cd9477023cd74d51c9fef4ef43387af90347ea
John Hall <jkhall@wpi.edu>
2025-03-03 21:58:25 -0500

    removed build from root dir

--- build/services/CMakeFiles/services.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/services"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/services.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- install/services/share/services/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(services_IDL_FILES "srv/IMUData.idl")
-set(services_INTERFACE_FILES "srv/IMUData.srv")
