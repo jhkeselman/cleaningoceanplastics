a9c6f498a25ba6042e5a800e837ed79ee69690e9
John Hall <jkhall@wpi.edu>
2025-03-08 16:30:33 -0500

    technically addedkalman

--- ros2_ws/src/sensors/sensors/Kalman_service.py
+++ ros2_ws/src/sensors/sensors/Kalman_service.py
@@ -5,9 +5,10 @@ import math
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Float64, Bool
-from sensor_msgs.msg import NavSatFix
-from services.srv import IMUData, GPSdata, KalmanState
+from std_msgs.msg import Float64, Bool, Float64MultiArray
+from sensor_msgs.msg import NavSatFix, Imu
+from services.srv import KalmanState
+from tf_transformations import euler_from_quaternion
 
 import math
 import numpy as np
@@ -15,6 +16,9 @@ import numpy as np
 RADIUS = 6371000
 UERE = 4.0
 AVERAGE = 10
+MASS = 23 #NEED VALUES
+DRAG = 0.5
+INERTIA = 200 
 
 class KalmanService(Node):
 
@@ -22,15 +26,9 @@ class KalmanService(Node):
         super().__init__('Kalman_service')
         self.srv = self.create_service(KalmanState, 'get_Kalman_state', self.return_state)
 
-        self.imu_cli = self.create_client(IMUData, 'get_IMU_data')
-        while not self.imu_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('IMU service not available, waiting again...')
-        self.imu_req = IMUData.Request()
-
-        self.gps_cli = self.create_client(GPSdata, 'get_GPS_fix')
-        while not self.gps_cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('GPS service not available, waiting again...')
-        self.gps_req = GPSdata.Request()
+        self.imu_sub = self.create_subscription(Imu,'IMU_data',self.imu_response_callback)
+        self.gps_sub = self.create_subscription(NavSatFix,'get_GPS',self.gps_response_callback)
+        self.motor_sub = self.create_subscription(Float64MultiArray, 'motor_speeds', self.motor_callback)
 
         self.emergency_stop = self.create_subscription(
             Bool,
@@ -38,66 +36,93 @@ class KalmanService(Node):
             self.destroy_node,
             10
         )
-
         
         self.avg_pos = np.zeros((AVERAGE,2))
         self.avg_i = 0
-        imu_timer_period = 0.25
-        gps_timer_period = 0.5
-        self.timer = self.create_timer(imu_timer_period, self.get_imu_data)
-        self.timer = self.create_timer(gps_timer_period, self.get_gps_data)
+        self.gps_ready = False
+        self.state = np.zeros((5,1)) #x,y,v,theta,omega
+        self.covariance = np.zeros((5,5))
+        
+        self.dt = 0.1
+        self.timer = self.create_timer(self.dt,self.calc_state)
+
+        R = 0.1 * np.ones((5,5)) #model noise
+        Q = 0.1 * np.ones((5,5)) #sensor noise
+        self.sensor_data = np.zeros((5,1))
+    
+    def calc_state(self):
+        #NON-LINEAR STATE PREDICTION
+        state_pred = np.zeros((5,1))
+        state_pred[0] = self.state[0] + self.state[2]*math.cos(self.state[3])*self.dt + (self.Tl + self.Tr - (DRAG*self.state[2]**2)*math.cos(self.state[3])*self.dt**2)/(2*MASS)
+        state_pred[1] = self.state[1] + self.state[2]*math.sin(self.state[3])*self.dt + (self.Tl + self.Tr - (DRAG*self.state[2]**2)*math.sin(self.state[3])*self.dt**2)/(2*MASS)
+        state_pred[2] = (self.Tl + self.Tr - (DRAG*self.state[2]**2)*self.dt)/MASS + self.state[2]
+        state_pred[3] = self.state[4]*self.dt + self.state[3]
+        state_pred[4] = (self.Tl + self.Tr - (1.25*DRAG*self.state[2]**2)*self.dt)/INERTIA + self.state[4] #drag increased by 25% for rotation
+
+        G = np.array([[1,0,(math.cos(self.state[3])*self.dt - (self.dt**2)*DRAG*self.state[2]/MASS),(-self.state[2]*math.sin(self.state[3]) - (self.Tl + self.Tr - (DRAG*self.state[2]**2)*math.sin(self.state[3])*self.dt**2)/(2*MASS)),0],
+                      [0,1,(math.sin(self.state[3])*self.dt - (self.dt**2)*DRAG*self.state[2]/MASS),(self.state[2]*math.cos(self.state[3]) + (self.Tl + self.Tr - (DRAG*self.state[2]**2)*math.cos(self.state[3])*self.dt**2)/(2*MASS)),0],
+                      [0,0,(-2*self.dt*DRAG*self.state[2]/MASS + 1),0,0],
+                      [0,0,0,1,self.dt],
+                      [0,0,0,0,1]])
+        
+        covariance_pred = np.matmul(G,np.matmul(self.covariance,G.T)) + self.R
+
+        #CORRECTION
+        H = np.array([[1,0,0,0,0],
+                      [0,1,0,0,0],
+                      [0,0,(self.Tl + self.Tr - (DRAG*self.state[2]**2))/MASS,0,0],
+                      [0,0,0,1,0],
+                      [0,0,0,0,1]])
+        
+        K = np.matmul(covariance_pred,np.matmul(H.T,np.inv(np.matmul(H,np.matmul(covariance_pred,H.T))+self.Q)))
+        sensor_model = self.state
+        sensor_model[2] = (self.Tl + self.Tr - (DRAG*self.state[2]**2))/MASS
+        self.state = state_pred + np.matmul(K,(self.sensor_data - sensor_model))
+        self.covariance = np.matmul((np.eye(5) - np.mapmul(K,H)),covariance_pred)
 
     def return_state(self, request, response):
         response.state = np.zeros(5)
         self.get_logger().info('Incoming request')
         return response
     
-    def get_imu_data(self):
-        self.imu_future = self.imu_cli.call_async(self.imu_req)
-        self.imu_future.add_done_callback(self.imu_response_callback)
+    def motor_callback(self,msg):
+        self.Tl = msg.data[0]
+        self.Tr = msg.data[1]
 
-    def imu_response_callback(self,future):
-        try:
-            response = future.result()
-            self.get_logger().info('IMU Heading %5.3f, Acc %5.3f, Omega %5.3f:' %(response.heading, response.acceleration, response.omega))
-        except Exception as e:
-            self.get_logger().error(f'Service call failed {str(e)}')
+    def imu_response_callback(self,msg):
+        (roll,pitch,yaw) = euler_from_quaternion(msg.orientation)
+        acc = msg.linear_acceleration.x
+        omega = msg.angular_velocity.z
+        self.get_logger().info('IMU Heading %5.3f, Acc %5.3f, Omega %5.3f:' %(yaw, acc, omega))
 
-    def get_gps_data(self):
-        self.gps_future = self.gps_cli.call_async(self.gps_req)
-        self.gps_future.add_done_callback(self.gps_response_callback)
-
-    def gps_response_callback(self,future):
-        try:
-            response = future.result()
-            fix_status = response.fix.status.status
-            if fix_status >=0 :
-                self.get_logger().info('GPS Fix %d, Lat %5.8f, Long %5.8f:' %(fix_status, response.fix.latitude, response.fix.longitude))
-                self.get_logger().info('GPS Covariance Long %5.3f, Lat %5.2f' %(response.fix.position_covariance[0], response.fix.position_covariance[0]))
-                if self.avg_i < AVERAGE-1:
-                    fix = [math.radians(response.fix.latitude), math.radians(response.fix.longitude)]
-                    self.avg_pos[self.avg_i,:] = fix    
-                    self.avg_i += 1               
-                elif self.avg_i == AVERAGE-1:
-                    fix = [math.radians(response.fix.latitude), math.radians(response.fix.longitude)]
-                    self.avg_pos[self.avg_i,:] = fix
-                    avg_lat = self.avg_pos[:,0].mean(axis=0)
-                    avg_lon = self.avg_pos[:,1].mean(axis=0)
-                    self.first_fix = [avg_lat,avg_lon,math.cos(avg_lat)]
-                    self.covariance = self.calc_covariance(response.fix)
-                    self.dx = 0
-                    self.dy = 0
-                    self.get_logger().info('Position (X,Y): (%5.3f +/- %5.3f, %5.3f +/- %5.3f)' %(self.dx,self.covariance[0][0],self.dy,self.covariance[1][1]))  
-                    self.avg_i += 1
-                else:
-                    [self.dx,self.dy] = self.calc_dist(response.fix)
-                    self.covariance = self.calc_covariance(response.fix)
-                    self.get_logger().info('Position (X,Y): (%5.3f +/- %5.3f, %5.3f +/- %5.3f)' %(self.dx,self.covariance[0][0],self.dy,self.covariance[1][1]))  
+    def gps_response_callback(self,msg):
+        fix_status = msg.status.status
+        if fix_status >=0 :
+            self.get_logger().info('GPS Fix %d, Lat %5.8f, Long %5.8f:' %(fix_status, msg.fix.latitude, msg.fix.longitude))
+            self.get_logger().info('GPS Covariance Long %5.3f, Lat %5.2f' %(msg.fix.position_covariance[0], msg.fix.position_covariance[0]))
+            if self.avg_i < AVERAGE-1:
+                fix = [math.radians(msg.fix.latitude), math.radians(msg.fix.longitude)]
+                self.avg_pos[self.avg_i,:] = fix    
+                self.avg_i += 1               
+            elif self.avg_i == AVERAGE-1:
+                fix = [math.radians(msg.fix.latitude), math.radians(msg.fix.longitude)]
+                self.avg_pos[self.avg_i,:] = fix
+                avg_lat = self.avg_pos[:,0].mean(axis=0)
+                avg_lon = self.avg_pos[:,1].mean(axis=0)
+                self.first_fix = [avg_lat,avg_lon,math.cos(avg_lat)]
+                self.covariance = self.calc_covariance(msg.fix)
+                self.dx = 0
+                self.dy = 0
+                self.get_logger().info('Position (X,Y): (%5.3f +/- %5.3f, %5.3f +/- %5.3f)' %(self.dx,self.covariance[0][0],self.dy,self.covariance[1][1]))  
+                self.avg_i += 1
+                self.gps_ready = True
             else:
-                    self.get_logger().info('No GPS Fix')
+                [self.dx,self.dy] = self.calc_dist(msg.fix)
+                self.covariance = self.calc_covariance(msg.fix)
+                self.get_logger().info('Position (X,Y): (%5.3f +/- %5.3f, %5.3f +/- %5.3f)' %(self.dx,self.covariance[0][0],self.dy,self.covariance[1][1]))  
+        else:
+                self.get_logger().info('No GPS Fix')
 
-        except Exception as e:
-            self.get_logger().error(f'Service call failed {str(e)}')
     def calc_dist(self,fix):
         dx = RADIUS*(math.radians(fix.longitude) - self.first_fix[1])*self.first_fix[2]
         dy = RADIUS*(math.radians(fix.latitude) - self.first_fix[0])
