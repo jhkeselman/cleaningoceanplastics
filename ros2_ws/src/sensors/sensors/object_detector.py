ccc745b8c1090b366ac86e704e78654b0b46f411
jhkeselman <jhkeselman@wpi.edu>
2025-03-17 14:10:39 -0400

    oops i typed .pt twice because i have brain damage

--- ros2_ws/src/sensors/sensors/object_detector.py
+++ ros2_ws/src/sensors/sensors/object_detector.py
@@ -8,7 +8,7 @@ class ObjectDetector(Node):
     def __init__(self):
         super().__init__('object_detector')
 
-        self.model = YOLO('ros2_ws/src/sensors/sensors/best.pt.pt')
+        self.model = YOLO('ros2_ws/src/sensors/sensors/best.pt')
         self.cap = cv2.VideoCapture(0)
         if not self.cap.isOpened():
             self.get_logger().error("Error: Could not open webcam.")
