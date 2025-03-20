8d2d5e269da4bfc1bd40d75d49a619b698861bd6
John Hall <jkhall@wpi.edu>
2025-03-20 16:02:10 -0400

    print

--- ros2_ws/src/sensors/sensors/imu_publisher.py
+++ ros2_ws/src/sensors/sensors/imu_publisher.py
@@ -165,7 +165,7 @@ class IMUPub(Node):
         self.avg_data[0,2] = (ACCy * 0.244/1000 * 9.81) + self.acc_bias
 
         self.omega, self.heading, self.acceleration = self.calc_avg()
-        print(self.calc_avg())
+        print(self.omega)
 
         imu_msg = Imu()
         imu_msg.header.frame_id = 'imu_pub'
