87ad038b89611d573223e5c000339f12491b0039
John Hall <jkhall@wpi.edu>
2025-03-21 15:48:17 -0400

    k = 0.15

--- ros2_ws/src/sensors/sensors/imu_publisher.py
+++ ros2_ws/src/sensors/sensors/imu_publisher.py
@@ -20,7 +20,7 @@ from .IMU_lib import *
 RAD_TO_DEG = 57.29578
 M_PI = 3.14159265358979323846
 G_GAIN = 0.070  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
-K =  0.1     # Complementary filter constant gain
+K =  0.15     # Complementary filter constant gain
 E = 0.0001     # Complementary filter bias gain
 MAX_DATA = 32767 
 
