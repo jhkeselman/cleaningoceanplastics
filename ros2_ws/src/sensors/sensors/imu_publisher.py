23e65ad8764c322e164429ec2ef3322de3768c54
John Hall <jkhall@wpi.edu>
2025-03-21 19:30:01 -0400

    printing smooth

--- ros2_ws/src/sensors/sensors/imu_publisher.py
+++ ros2_ws/src/sensors/sensors/imu_publisher.py
@@ -171,7 +171,7 @@ class IMUPub(Node):
             np.savetxt("Heading_data.csv",self.csv_data,delimiter = ',')
             print("got here")
             self.i += 1
-        self.get_logger().info("Gyro: %5.3f  Mag: %5.3f  CF: %5.3f" %(self.gyro_heading,mag_heading,math.degrees(heading)))
+        self.get_logger().info("Gyro: %5.3f  Mag: %5.3f  CF: %5.3f" %(self.gyro_heading,mag_heading,self.heading))
 
         imu_msg = Imu()
         imu_msg.header.frame_id = 'imu_pub'
