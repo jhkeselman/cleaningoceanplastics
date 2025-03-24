84ed4f9b99956f1f47a10ff002f79c4a38561376
John Hall <jkhall@wpi.edu>
2025-03-24 15:16:54 -0400

    printing pred

--- ros2_ws/src/sensors/sensors/Kalman_service.py
+++ ros2_ws/src/sensors/sensors/Kalman_service.py
@@ -64,6 +64,8 @@ class KalmanService(Node):
         state_pred[3,0] = self.state[4]*self.dt + self.state[3,0]
         state_pred[4,0] = (self.Tl + self.Tr - (1.25*DRAG*self.state[2,0]**2)*self.dt)/INERTIA + self.state[4,0] #drag increased by 25% for rotation
 
+        print(state_pred[2,0])
+
         G = np.array([[1,0,(math.cos(self.state[3,0])*self.dt - (self.dt**2)*DRAG*self.state[2,0]*math.cos(self.state[3,0])/MASS),(-self.state[2,0]*math.sin(self.state[3,0])*self.dt - (self.Tl + self.Tr - (DRAG*self.state[2,0]**2)*math.sin(self.state[3,0])*self.dt**2)/(2*MASS)),0],
                       [0,1,(math.sin(self.state[3,0])*self.dt - (self.dt**2)*DRAG*self.state[2,0]*math.sin(self.state[3,0])/MASS),(self.state[2,0]*math.cos(self.state[3,0])*self.dt + (self.Tl + self.Tr - (DRAG*self.state[2,0]**2)*math.cos(self.state[3,0])*self.dt**2)/(2*MASS)),0],
                       [0,0,(-2*self.dt*DRAG*self.state[2,0]/MASS + 1),0,0],
@@ -90,7 +92,7 @@ class KalmanService(Node):
         self.covariance = np.matmul((np.eye(5) - np.matmul(K,H)),covariance_pred)
 
         msg = Float64MultiArray()
-        print(G)
+        print(self.state[2,0])
         msg.data = self.state
         self.pub.publish(msg)
 
