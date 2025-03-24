e6f8a581e9e7d034b1d99c3fcc164f9f3d1a3cd9
jhkeselman <jhkeselman@wpi.edu>
2025-03-24 15:27:13 -0400

    code for publishing images

--- ros2_ws/src/sensors/sensors/object_detector.py
+++ ros2_ws/src/sensors/sensors/object_detector.py
@@ -16,15 +16,18 @@ class ObjectDetector(Node):
             self.get_logger().error("Error: Could not open webcam.")
             self.destroy_node()
             return
+        
+        self.save_video = True # Set to False to disable video recording
 
-        frame_width = int(self.cap.get(3))
-        frame_height = int(self.cap.get(4))
+        if self.save_video:
+            frame_width = int(self.cap.get(3))
+            frame_height = int(self.cap.get(4))
 
-        timestamp = time.strftime("%Y%m%d_%H%M%S")
-        self.video_filename = f"detection_{timestamp}.mp4"
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        fps = 5
-        self.out = cv2.VideoWriter(self.video_filename, fourcc, fps, (frame_width, frame_height))
+            timestamp = time.strftime("%Y%m%d_%H%M%S")
+            self.video_filename = f"detection_{timestamp}.mp4"
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            fps = 5
+            self.out = cv2.VideoWriter(self.video_filename, fourcc, fps, (frame_width, frame_height))
         
         self.detection_pub = self.create_publisher(String, 'object_detections', 10)
         
@@ -36,17 +39,37 @@ class ObjectDetector(Node):
         if ret:
             frame = cv2.flip(frame, 0)
             results = self.model(frame, verbose=True)
-            annotated_frame = results[0].plot()  # YOLO outputs annotated images with .plot()
 
-            self.out.write(frame)
-            cv2.imshow("Real-Time Detection w/ YOLO", annotated_frame)
+            if self.save_video:
+                annotated_frame = results[0].plot()  # YOLO outputs annotated images with .plot()
+                self.out.write(frame)
+                cv2.imshow("Real-Time Detection w/ YOLO", annotated_frame)
+
+            publishString = String()
+            objectStrings = ""
+
+            for box in results[0].boxes:
+                class_label = self.model.names[int(box.cls[0])]
+                raw_conf = box.conf[0]
+                conf = 100 * round(raw_conf.item(), 2)
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                objectString = f"{class_label}, {conf}, {x1}, {y1}, {x2}, {y2}"
+                if objectStrings:
+                    objectStrings += " $ " + objectString
+                else:
+                    objectStrings = objectString
+
+            if objectStrings != "":
+                publishString.data = objectStrings
+            self.detection_pub.publish(publishString)
             cv2.waitKey(1)
         
 
     def destroy_node(self):
         if self.cap.isOpened():
             self.cap.release()
-            self.out.release()
+            if self.save_video:
+                self.out.release()
             cv2.destroyAllWindows()
         super().destroy_node()
 
