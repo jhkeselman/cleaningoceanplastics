c4d6b0d7afb799e5e52a1c553b18e072db3b1251
John Hall <jkhall@wpi.edu>
2025-03-25 13:32:47 -0400

    trying

--- ros2_ws/src/sensors/sensors/Kalman_service.py
+++ ros2_ws/src/sensors/sensors/Kalman_service.py
@@ -67,16 +67,16 @@ class KalmanService(Node):
 
         G = np.array([[1,0,(math.cos(self.state[3,0])*self.dt - (self.dt**2)*DRAG*self.state[2,0]*math.cos(self.state[3,0])/MASS),(-self.state[2,0]*math.sin(self.state[3,0])*self.dt - (self.Tl + self.Tr - (DRAG*self.state[2,0]**2)*math.sin(self.state[3,0])*self.dt**2)/(2*MASS)),0],
                       [0,1,(math.sin(self.state[3,0])*self.dt - (self.dt**2)*DRAG*self.state[2,0]*math.sin(self.state[3,0])/MASS),(self.state[2,0]*math.cos(self.state[3,0])*self.dt + (self.Tl + self.Tr - (DRAG*self.state[2,0]**2)*math.cos(self.state[3,0])*self.dt**2)/(2*MASS)),0],
-                      [0,0,(-np.sign(self.state[2,0])*2*self.dt*DRAG*self.state[2,0]/MASS + 1),0,0],
+                      [0,0,(-1*np.sign(self.state[2,0])*2*self.dt*DRAG*self.state[2,0]/MASS + 1),0,0],
                       [0,0,0,1,self.dt],
-                      [0,0,0,0,(-np.sign(self.state[4,0])*2*self.dt*1.25*DRAG*(self.state[4,0]**2)/INERTIA + 1)]],np.float64)
+                      [0,0,0,0,(-1*np.sign(self.state[4,0])*2*self.dt*1.25*DRAG*(self.state[4,0]**2)/INERTIA + 1)]],np.float64)
 
         covariance_pred = np.matmul(G,np.matmul(self.covariance,G.T)) + self.R
 
         #CORRECTION
         H = np.array([[1,0,0,0,0],
                       [0,1,0,0,0],
-                      [0,0,(-np.sign(self.state[2,0])*2*DRAG*self.state[2,0])/MASS,0,0],
+                      [0,0,(-1*np.sign(self.state[2,0])*2*DRAG*self.state[2,0])/MASS,0,0],
                       [0,0,0,1,0],
                       [0,0,0,0,1]],np.float64)
         
