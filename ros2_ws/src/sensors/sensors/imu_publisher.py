919c8a3b4977edefd11a8890509706dbbdf2f880
John Hall <jkhall@wpi.edu>
2025-04-01 15:05:31 -0400

    switching heading

--- ros2_ws/src/sensors/sensors/imu_publisher.py
+++ ros2_ws/src/sensors/sensors/imu_publisher.py
@@ -58,8 +58,6 @@ class IMUPub(Node):
 
         self.avg_data = MAX_DATA*np.ones((20,4)) #Acc, Gyro, X-heading, Y-heading
 
-        self.i = 0
-
         self.emergency_stop = self.create_subscription(
             Bool,
             'emergency_stop',
@@ -130,7 +128,7 @@ class IMUPub(Node):
         ang_vel = rate_gyr_x - self.gyro_bias
 
         #Calculate heading
-        mag_heading = math.degrees(math.atan2(MAGy,MAGz))
+        mag_heading = math.degrees(math.atan2(-MAGz,MAGy))
         
         mag_heading += self.declination
 
