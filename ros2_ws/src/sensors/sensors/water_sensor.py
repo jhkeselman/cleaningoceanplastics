dc63d1a14132ad9a6a26915de189fcded2d2d272
Cooper Mann <coop530@comcast.net>
2025-04-04 15:46:19 -0400

    fix before merge

--- ros2_ws/src/sensors/sensors/water_sensor.py
+++ ros2_ws/src/sensors/sensors/water_sensor.py
@@ -9,7 +9,7 @@ class WaterSensor(Node):
 
     def __init__(self):
         super().__init__('water_sensor')
-        self.publisher_ = self.create_publisher(Bool, 'water_detected', 10)
+        self.publisher_ = self.create_publisher(Bool, 'emergency_stop', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
