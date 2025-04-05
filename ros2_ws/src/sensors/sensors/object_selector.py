edf9250506c3df56701f005c0adda18583665ee6
jhkeselman <jhkeselman@wpi.edu>
2025-04-05 18:17:26 -0400

    Comments for object_detector and fixed publishing of empty strings

--- ros2_ws/src/sensors/sensors/object_selector.py
+++ ros2_ws/src/sensors/sensors/object_selector.py
@@ -45,34 +45,33 @@ class ObjectSelector(Node):
         xCenters = []
         yCenters = []
         # If there are detected objects, calculate their centers
-        if objects != ['']:
-            for object in objects:
-                # Split each object string into its components
-                components = object.split(", ")
-                object_type = components[0]
-                confidence = components[1]
-                # x1: top left x coordinate
-                x1 = components[2]
-                # y1: top left y coordinate
-                y1 = components[3]
-                # x2: bottom right x coordinate
-                x2 = components[4]
-                # y2: bottom right y coordinate
-                y2 = components[5]
-                # Add the center of the object to the lists, using statistics.mean to calculate the average of the x and y coordinates
-                xCenters.append(statistics.mean([float(x1), float(x2)]))
-                yCenters.append(statistics.mean([float(y1), float(y2)]))
+        for object in objects:
+            # Split each object string into its components
+            components = object.split(", ")
+            object_type = components[0]
+            confidence = components[1]
+            # x1: top left x coordinate
+            x1 = components[2]
+            # y1: top left y coordinate
+            y1 = components[3]
+            # x2: bottom right x coordinate
+            x2 = components[4]
+            # y2: bottom right y coordinate
+            y2 = components[5]
+            # Add the center of the object to the lists, using statistics.mean to calculate the average of the x and y coordinates
+            xCenters.append(statistics.mean([float(x1), float(x2)]))
+            yCenters.append(statistics.mean([float(y1), float(y2)]))
 
-            # Calculate the centroid by averaging the x and y coordinates of all detected objects
-            x = statistics.mean(xCenters)
-            y = statistics.mean(yCenters)
+        # Calculate the centroid by averaging the x and y coordinates of all detected objects
+        x = statistics.mean(xCenters)
+        y = statistics.mean(yCenters)
 
-            # Creates a Float64MultiArray message to publish the centroid
-            msg = Float64MultiArray()
-            msg.data = [x, y]
-            # Publish the centroid message to 'centroid'
-            self.centroid_publisher.publish(msg)
-            self.get_logger().info(f"Centroid: {x}, {y}")
+        # Creates a Float64MultiArray message to publish the centroid
+        msg = Float64MultiArray()
+        msg.data = [x, y]
+        # Publish the centroid message to 'centroid'
+        self.centroid_publisher.publish(msg)
+        self.get_logger().info(f"Centroid: {x}, {y}")
 
     # Destroy the node when the emergency stop is triggered
     def destroy_node(self,msg):
