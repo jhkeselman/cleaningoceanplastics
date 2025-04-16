1b1b900a77a5965387285f9323d4e0833874b118
Cooper Mann <coop530@comcast.net>
2025-04-16 11:35:55 -0400

    test

--- ros2_ws/src/sensors/sensors/object_selector.py
+++ ros2_ws/src/sensors/sensors/object_selector.py
@@ -51,8 +51,8 @@ class ObjectSelector(Node):
             object_type = components[0]
             if object_type == "Bottle" or object_type == "Can":
                 confidence = components[1]
-                if confidence < 30:
-                    continue
+                # if confidence < 30:
+                #     continue
                 # x1: top left x coordinate
                 x1 = components[2]
                 # y1: top left y coordinate
